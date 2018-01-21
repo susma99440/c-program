9460036d8fb6152656da0f94c667a7af3ce404f6
susma99440 <31506083+susma99440@users.noreply.github.com>
2018-01-21 11:50:10 +0530

    Create progr20.c

---
+++ progr20.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,f,a;
+printf("Enter the number");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+f=a*i;
+printf("the first five multiples of five is %d",f);
+}
+return 0;
+}
