e5452522d5b14ab699480cb990234ce407bea1b3
susma99440 <31506083+susma99440@users.noreply.github.com>
2018-01-31 15:52:40 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+	int f=0,x;
+	printf("enter the value ");
+	scanf("%d",&x);
+	if(x<=10)
+	{
+	f=1;	
+	}
+	if(f==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	
+}
