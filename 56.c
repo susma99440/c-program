01a11737ae8c8eab0765df76bf8137e61129b4a7
susma99440 <31506083+susma99440@users.noreply.github.com>
2018-02-09 16:28:09 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,a;
+	char ch[100];
+	scanf("%d",&a);
+	if((ch[i]>='A'||ch[i]<='Z')&&(ch[i]>='a'||ch[i]<='z')&&(ch[i]>='0'||ch[i]<='9'))
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
