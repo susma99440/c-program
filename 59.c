b11b3664024d4ab3354d3c0d6d0b874fc1f3acee
susma99440 <31506083+susma99440@users.noreply.github.com>
2018-02-12 16:41:04 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+
+	int a[10],i,j,t,n=10;
+
+	for(i=0;i<n;i++)
+
+	{
+
+		scanf("%d",&a[i]);
+
+	}
+
+	for(i=0;i<n;i++)
+
+	{
+
+		for(j=i+1;j<n;j++)
+
+		if(a[i] < a[j])
+
+		{
+
+			t=a[i];
+
+			a[i]=a[j];
+
+			a[j]=t;
+
+			
+
+		}
+
+	}
+
+	printf("%d",a[0]);
+
+	return 0;
+
+}
