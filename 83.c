54f24ae469bb7a5b137ab63c6a536ad4c46a38bd
susma99440 <31506083+susma99440@users.noreply.github.com>
2018-03-14 16:48:16 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c,d,f,g,h,i,j,k;
+ scanf("%d %d",&a,&b);
+ scanf("%d %d",&d,&f);
+ scanf("%d %d",&h,&i);
+ c=a/b;
+ g=d%f;
+ j=h/i;
+ k=h%i;
+printf("\n%d",c);
+printf("\n%d",g);
+printf("\n%d",j);
+printf("\n%d",k);
+return 0;
+}
