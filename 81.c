36ada0d00efe900528785037f6331df24460fdef
susma99440 <31506083+susma99440@users.noreply.github.com>
2018-03-14 16:20:39 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c,d,f,g,h,i,j;
+ scanf("%d %d",&a,&b);
+ scanf("%d %d",&d,&f);
+ scanf("%d %d",&h,&i);
+ c=b-a;
+ g=f-d;
+ j=i-h;
+printf("\n%d",c);
+printf("\n%d",g);
+printf("\n%d",j);
+return 0;
+}
